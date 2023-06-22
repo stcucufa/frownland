867f1a33da03a6f84f61db889255428d7b9f8be4
stcucufa <githubnyou@romulusetrem.us>
2023-06-22 11:50:36 +0200

    Basic connections

--- patcher/patcher.js
+++ patcher/patcher.js
@@ -1,7 +1,10 @@
-import { create, svg } from "../lib/util.js";
+import { assign, create, svg } from "../lib/util.js";
 
 // The main canvas element
 const canvas = document.querySelector("svg.canvas");
+
+// Map between DOM elements and the objects that they represent on the canvas.
+const Elements = new Map();
 
 // Event listener for the main document to handle keyboard commands.
 const DocumentEventListener = {
@@ -18,8 +21,7 @@ const DocumentEventListener = {
             case "keyup":
                 if (event.key === "n") {
                     // N for new box
-                    const box = Box({ x: this.x, y: this.y });
-                    canvas.appendChild(box.element).addEventListener("pointerdown", DragEventListener);
+                    canvas.appendChild(Box({ x: this.x, y: this.y }).element);
                 }
         }
     }
@@ -28,7 +30,7 @@ const DocumentEventListener = {
 document.addEventListener("pointermove", DocumentEventListener);
 document.addEventListener("keyup", DocumentEventListener);
 
-// Drag event listener for boxes.
+// Drag event listener for boxes and cords.
 const DragEventListener = {
     handleEvent(event) {
         switch (event.type) {
@@ -37,29 +39,24 @@ const DragEventListener = {
                 document.addEventListener("pointerup", this);
                 document.addEventListener("pointercancel", this);
                 event.preventDefault();
-                this.box = Box.elements.get(event.currentTarget);
-                this.x = this.box.x;
-                this.y = this.box.y;
                 this.x0 = event.clientX;
                 this.y0 = event.clientY;
-                this.box.bringToFront();
+                this.target = Elements.get(event.currentTarget);
+                this.target.dragDidBegin?.(this.x0, this.y0);
                 break;
             case "pointermove":
-                this.box.x = this.x + event.clientX - this.x0;
-                this.box.y = this.y + event.clientY - this.y0;
-                this.box.updatePosition();
+                const x = event.clientX;
+                const y = event.clientY;
+                this.target.dragDidProgress?.(x - this.x0, y - this.y0, x, y);
                 break;
             case "pointercancel":
-                this.box.x = this.x;
-                this.box.y = this.y;
-                this.box.updatePosition();
+                this.target.dragWasCancelled?.();
                 // Fallthrough
             case "pointerup":
+                this.target.dragDidEnd?.();
                 delete this.x0;
                 delete this.y0;
-                delete this.x;
-                delete this.y;
-                delete this.box;
+                delete this.target;
                 document.removeEventListener("pointermove", this);
                 document.removeEventListener("pointerup", this);
                 document.removeEventListener("pointercancel", this);
@@ -68,9 +65,7 @@ const DragEventListener = {
 };
 
 // Ports are inlets and outlets.
-const Port = Object.assign(properties => create(properties).call(Port), {
-    elements: new Map(),
-
+const Port = assign(properties => create(properties).call(Port), {
     x: 0,
     y: 0,
     width: 12,
@@ -78,14 +73,110 @@ const Port = Object.assign(properties => create(properties).call(Port), {
 
     init() {
         this.element = svg("rect", { width: this.width, height: this.height, x: this.x, y: this.y });
-        this.elements.set(this, this.element);
+        this.element.addEventListener("pointerdown", DragEventListener);
+        Elements.set(this.element, this);
+        this.cords = [];
+    },
+
+    get centerX() {
+        return this.box.x + this.x + this.width / 2;
+    },
+
+    get centerY() {
+        return this.box.y + this.y + this.height / 2;
+    },
+
+    updateCords() {
+        if (this.isOutlet) {
+            for (const cord of this.cords) {
+                cord.setAttribute("x1", this.centerX);
+                cord.setAttribute("y1", this.centerY);
+            }
+        } else {
+            for (const cord of this.cords) {
+                cord.setAttribute("x2", this.centerX);
+                cord.setAttribute("y2", this.centerY);
+            }
+        }
+    },
+
+    get isTargetForCord() {
+        return this.element.classList.contains("target");
+    },
+
+    set isTargetForCord(value) {
+        this.element.classList.toggle("target", value);
+    },
+
+    possibleTargetForCord(port) {
+        if (port.box !== this.box && port.isOutlet !== this.isOutlet) {
+            return { port: this, x: this.centerX, y: this.centerY };
+        }
+    },
+
+    dragDidBegin(x, y) {
+        this.cord = this.box.element.parentElement.appendChild(svg("line", {
+            stroke: this.box.foregroundColor,
+            x1: this.centerX,
+            y1: this.centerY,
+            x2: x,
+            y2: y,
+        }));
+    },
+
+    dragDidProgress(_, __, x, y) {
+        const element = document.elementsFromPoint(x, y)[1];
+        const target = Elements.get(element)?.possibleTargetForCord?.(this);
+        if (target) {
+            this.cord.setAttribute("x2", target.x);
+            this.cord.setAttribute("y2", target.y);
+            if (this.target !== target.port) {
+                if (this.target) {
+                    this.target.isTargetForCord = false;
+                }
+                this.target = target.port;
+                this.target.isTargetForCord = true;
+            }
+        } else {
+            this.cord.setAttribute("x2", x);
+            this.cord.setAttribute("y2", y);
+            if (this.target) {
+                this.target.isTargetForCord = false;
+                delete this.target;
+            }
+        }
+    },
+
+    dragWasCancelled() {
+        if (this.target) {
+            this.target.isTargetForCord = false;
+            delete this.target;
+        }
+    },
+
+    dragDidEnd() {
+        if (this.target) {
+            this.target.isTargetForCord = false;
+            this.cords.push(this.cord);
+            this.target.cords.push(this.cord);
+            if (!this.isOutlet) {
+                this.cord.setAttribute("x1", this.target.centerX);
+                this.cord.setAttribute("y1", this.target.centerY);
+                this.cord.setAttribute("x2", this.centerX);
+                this.cord.setAttribute("y2", this.centerY);
+            }
+            delete this.target;
+        } else {
+            this.cord.remove();
+        }
+        delete this.cord;
+        delete this.x0;
+        delete this.y0;
     }
 });
 
 // A box represents an object. It has two inlets and one outlet by default.
 const Box = Object.assign(properties => create(properties).call(Box), {
-    elements: new Map(),
-
     x: 0,
     y: 0,
     width: 104,
@@ -94,28 +185,60 @@ const Box = Object.assign(properties => create(properties).call(Box), {
     backgroundColor: "#f8f9f0",
 
     init() {
-        const inlets = [Port(), Port({ x: this.width - Port.width })];
-        const outlet = Port({ y: this.height - Port.height });
+        this.inlets = [Port({ box: this }), Port({ box: this, x: this.width - Port.width })];
+        this.outlet = Port({ box: this, y: this.height - Port.height, isOutlet: true });
+        const rect = svg("rect", {
+            stroke: this.foregroundColor,
+            fill: this.backgroundColor,
+            width: this.width,
+            height: this.height
+        });
         this.element = svg("g",
-            svg("rect", {
-                stroke: this.foregroundColor,
-                fill: this.backgroundColor,
-                width: this.width,
-                height: this.height
-            }),
-            svg("g", { fill: this.foregroundColor }, inlets.map(inlet => inlet.element), outlet.element)
+            rect,
+            svg("g", { fill: this.foregroundColor },
+                this.inlets.map(inlet => inlet.element),
+                this.outlet.element
+            )
         );
+        rect.addEventListener("pointerdown", DragEventListener);
         this.updatePosition();
-        this.elements.set(this.element, this);
-    },
-
-    bringToFront() {
-        const parent = this.element.parentElement;
-        this.element.remove();
-        parent.appendChild(this.element);
+        Elements.set(rect, this);
     },
 
     updatePosition() {
         this.element.setAttribute("transform", `translate(${this.x}, ${this.y})`);
-    }
+        for (const port of [...this.inlets, this.outlet]) {
+            port.updateCords();
+        }
+    },
+
+    // Drag handling
+    dragDidBegin() {
+        bringElementFrontward(this.element);
+        this.x0 = this.x;
+        this.y0 = this.y;
+    },
+
+    dragDidProgress(dx, dy) {
+        this.x = this.x0 + dx;
+        this.y = this.y0 + dy;
+        this.updatePosition();
+    },
+
+    dragWasCancelled() {
+        this.x = this.x0;
+        this.y = this.y0;
+        this.updatePosition();
+    },
+
+    dragDidEnd() {
+        delete this.x0;
+        delete this.y0;
+    },
 });
+
+function bringElementFrontward(element) {
+    const parent = element.parentElement;
+    element.remove();
+    parent.appendChild(element);
+}
