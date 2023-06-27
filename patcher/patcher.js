38cf580b9f4e42c5f61df71cd42b971f132921a5
stcucufa <githubnyou@romulusetrem.us>
2023-06-27 08:57:16 +0200

    Esc to cancel drag
    Pressing Escape will cancel a drag gesture—intended to cancel drawing a new cord, but will also cancel a box move. Also disable keyboard commands while a drag gesture is in progress (so no creating a new box while dragging).

--- patcher/patcher.js
+++ patcher/patcher.js
@@ -8,6 +8,7 @@ const DragEventListener = {
                 document.addEventListener("pointermove", this);
                 document.addEventListener("pointerup", this);
                 document.addEventListener("pointercancel", this);
+                document.addEventListener("keyup", this);
                 event.preventDefault();
                 event.stopPropagation();
                 this.x0 = event.clientX;
@@ -24,14 +25,25 @@ const DragEventListener = {
                 this.target.dragWasCancelled?.();
                 // Fallthrough
             case "pointerup":
-                this.target.dragDidEnd?.();
-                delete this.x0;
-                delete this.y0;
-                delete this.target;
-                document.removeEventListener("pointermove", this);
-                document.removeEventListener("pointerup", this);
-                document.removeEventListener("pointercancel", this);
+                this.draggingDidEnd();
+                break;
+            case "keyup":
+                if (event.key === "Escape") {
+                    this.target.dragWasCancelled?.();
+                    this.draggingDidEnd();
+                }
         }
+    },
+
+    draggingDidEnd() {
+        this.target.dragDidEnd?.();
+        delete this.x0;
+        delete this.y0;
+        delete this.target;
+        document.removeEventListener("pointermove", this);
+        document.removeEventListener("pointerup", this);
+        document.removeEventListener("pointercancel", this);
+        document.removeEventListener("keyup", this);
     }
 };
 
@@ -433,7 +445,7 @@ export const App = {
             case "keyup":
                 // Execute the command associated with the key (when not
                 // otherwise editing).
-                if (!this.editItem) {
+                if (!this.editItem && !DragEventListener.target) {
                     this.commands[event.key]?.call(this);
                 }
                 break;
