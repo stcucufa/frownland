e246524da5c8032096e02eab652d339485078b0d
stcucufa <githubnyou@romulusetrem.us>
2023-08-14 18:41:59 +0200

    Buttons

--- patches/patches.js
+++ patches/patches.js
@@ -1,4 +1,4 @@
-import { assign, create, html } from "../lib/util.js";
+import { assign, create, html, remove } from "../lib/util.js";
 import { notify, on } from "../lib/events.js";
 
 // Manage a list of patches by their ID.
@@ -12,7 +12,7 @@ const Patches = assign(() => create().call(Patches), {
     // Attempt to get saved IDs from local storage.
     loadPatchIDs() {
         try {
-            const json = window.localStorage.getItem(this.localStorageKey);
+            const json = localStorage.getItem(this.localStorageKey);
             if (json) {
                 this.ids = JSON.parse(json);
             }
@@ -24,6 +24,19 @@ const Patches = assign(() => create().call(Patches), {
     // Add the ID of the new patch to the list and save it to local storage.
     addPatchID(id) {
         this.ids.push(id);
+        this.saveToLocalStorage();
+    },
+
+    // Remove the ID of a patch from the list and save it to local storage.
+    removePatchID(id) {
+        if (remove(this.ids, id)) {
+            this.saveToLocalStorage();
+        }
+    },
+
+    // Save the current list of ids to local storage, notifying the controller
+    // in case of errors.
+    saveToLocalStorage() {
         try {
             localStorage.setItem(this.localStorageKey, JSON.stringify(this.ids));
         } catch (error) {
@@ -32,33 +45,34 @@ const Patches = assign(() => create().call(Patches), {
     }
 });
 
+function button(label, f) {
+    const button = html("button", { type: "button" }, label);
+    button.addEventListener("click", f);
+    return button;
+}
+
 const PatchesView = assign(() => create().call(PatchesView), {
     init() {
-        window.addEventListener("keyup", this);
-        this.element = document.body.appendChild(html("ul", { class: "patches" }));
-    },
-
-    // Keyboard commands for different keys.
-    keyboardCommands: {
-        // Create a new patch.
-        n() {
-            notify(this, "new");
-        },
-    },
-
-    // Listen to keyboard commands.
-    handleEvent(event) {
-        switch (event.type) {
-            case "keyup":
-                this.keyboardCommands[event.key]?.call(this);
-                break;
-        }
+        this.ul = html("ul", { class: "patches" });
+        this.element = document.body.appendChild(html("div",
+            html("p", button("New", () => { notify(this, "new"); })),
+            this.ul
+        ));
     },
 
     addPatchID(id) {
-        const openButton = html("button", { type: "button" }, "Open");
-        openButton.addEventListener("click", () => { window.location = `../patcher/?id=${id}`; });
-        this.element.appendChild(html("li", `Patch #${id} `, openButton));
+
+        const openButton = button("open", () => { window.location = `../patcher/?id=${id}`; });
+        const deleteButton = button("delete", () => {
+            try {
+                localStorage.removeItem(`patch:${id}`);
+                item.remove();
+                notify(this, "remove", { id });
+            } catch (error) {
+                console.error(`Could not remove item "patch:${id}"`, error);
+            }
+        });
+        const item = this.ul.appendChild(html("li", `Patch #${id} `, openButton, " ", deleteButton));
     }
 });
 
@@ -69,6 +83,7 @@ const PatchesController = assign(() => create().call(PatchesController), {
 
         this.view = PatchesView();
         on(this.view, "new", () => { this.createPatch(); });
+        on(this.view, "remove", ({ id }) => { this.model.removePatchID(id); });
 
         this.model.loadPatchIDs();
         for (const id of this.model.ids) {
